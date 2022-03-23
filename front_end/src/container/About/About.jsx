600124b8c08cc70d697d78d02d7687b3ddb3c10f
shyrapson <meetshyrapson@gmail.com>
2022-03-23 14:40:25 +0100

    update

--- front_end/src/container/About/About.jsx
+++ front_end/src/container/About/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import { images } from "../../constants";
+
 import './About.scss'
 const abouts = [
-  {title:'Web Development', description:'I am a good web developer',},
-  {title:'Web Development', description:'I am a good web developer'},
-  {title:'Web Development', description:'I am a good web developer'},
+  {title:'Web Development', description:'I am a good web developer', imageUrl:images.about01,},
+  {title:'Web Animations', description:'I am a good web developer',imageUrl:images.about02}
 ]
 
 function About() {
@@ -24,7 +25,11 @@ function About() {
   whileHover={{scale:1.1}}
   transition={{duration:0.5, type:'tween'}}
   className="app__profile-item"
+  key={about.title + index}
   >
+    <img src={about.imageUrl} alt={about.title}/>
+    <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
+    <p className='p-text' style={{marginTop:10}}>{about.description}</p>
 
   </motion.div>
 ))}
